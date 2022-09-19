1b4c66a9cc8a404851f1630dd0d87b93773ddf70
Luz Iferato <105192630+k37z3r@users.noreply.github.com>
2022-09-19 12:05:50 +0200

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,18 @@
+import * as alt from 'alt';
+async function getOAuthToken(DISCORD_APP_ID) {
+    try {
+        const token = await alt.Discord.requestOAuth2Token(DISCORD_APP_ID);
+        alt.emitServer('bao:discord:token', token);
+    } catch (e) {
+        alt.logError(e);
+    }
+}
+alt.onServer('bao:discord:set_discord_app_id', (APP_ID) => {
+	getOAuthToken(APP_ID);
+});
+
+/*
+alt.onServer("discordbotbyluz:debuginfos", (infos) => {
+	console.log(infos);
+});
+*/
